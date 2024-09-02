c688f5a81c903719666270006e69f1a7ccfa1bd3
Tanzeela Fatima <149133736+Fatima-progmmer@users.noreply.github.com>
2024-09-02 11:01:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var countdown = new Date("January 16,2025 00:00:00").getTime();
+var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countdown - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+    document.getElementById("day").innerHTML = days;
+    document.getElementById("hour").innerHTML = hours;
+    document.getElementById("minute").innerHTML = minutes;
+    document.getElementById("second").innerHTML = seconds;
+
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("day").innerHTML = "00";
+        document.getElementById("hour").innerHTML = "00";
+        document.getElementById("minute").innerHTML = "00";
+        document.getElementById("second").innerHTML = "00";
+    }
+
+}, 1000);
